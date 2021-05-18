30247dcb185ac4b3d9aac3b9e87b4a25325b8b5d
Leonardo-Malcotti <Leo.Malcotti@gmail.com>
2021-05-18 22:39:41 +0200

    added content

--- ee.c
+++ ee.c
@@ -101,7 +101,19 @@ void enableRawMode() {
 /*--- MAIN --- */
 
 int main(int argc, char *argv[]){
+    enableRawMode();
 
-	return 0;
+    while(1){
+    char c ='\0';
+
+        if ( read(STDIN_FILENO, &c,1) == -1 && errno != EAGAIN) die("read");
+        if(iscntrl(c)){
+            printf("%d\r\n",c);
+        } else {
+            printf("%d ('%c')\r\n",c,c);
+        }
+        if(c==CTRL_KEY('q')) break;
+    }
+     return 0;
 }
 
